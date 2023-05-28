03393c89ae7ded92cdd0e7810fdc61c3f8147b2e
AaronTech112 <abdullahiaaron112@gmail.com>
2023-05-28 23:09:04 +0100

    db uplad product

--- shop/models.py
+++ shop/models.py
@@ -42,7 +42,7 @@ class Product(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField(blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    image = models.ImageField(upload_to='products/', default = '',blank=True)
+    image = models.ImageField(upload_to='products', default = '',blank=True)
     available = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now_add=True)
